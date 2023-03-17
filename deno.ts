3364c6de298850df499b43d0107e4909007935a0
Mariano Paporello <mariano.paporello@gmail.com>
2023-03-17 11:36:17 -0300

    deno routes

--- deno.ts
+++ deno.ts
@@ -10,5 +10,13 @@ app.post("/", (req: Request, res:Response)=>{
     res.send("HOLA DESDE POST A MAIN PAGE")
 })
 
+app.put("/", (req: Request, res:Response)=>{
+    res.send("HOLA DESDE PUT MAIN PAGE")
+})
+
+app.delete("/", (req:Request, res:Response)=>{
+    res.send("HOLA DESDE DELETE MAIN PAGE")
+})
+
 app.listen(8000)
 console.log(`Server up in ${8000}`)
